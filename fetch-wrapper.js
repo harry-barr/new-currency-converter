1ded7410f5aed934943f0b6756c2eee8aafcde66
Harry Barr <harrybarr530@gmail.com>
2024-08-27 11:18:56 +0100

    add

--- fetch-wrapper.js
+++ fetch-wrapper.js
@@ -6,26 +6,4 @@ export default class FetchWrapper {
   get(endpoint) {
     return fetch(this.baseURL + endpoint).then((response) => response.json());
   }
-
-  put(endpoint, body) {
-    return this.#send("put", endpoint, body);
-  }
-
-  post(endpoint, body) {
-    return this.#send("post", endpoint, body);
-  }
-
-  delete(endpoint, body) {
-    return this.#send("delete", endpoint, body);
-  }
-
-  #send(method, endpoint, body) {
-    return fetch(this.baseURL + endpoint, {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    }).then((response) => response.json());
-  }
 }
